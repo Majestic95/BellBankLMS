dee5ceccd165d1b9dfb66c4428fb751bdcd42492
Majestic95 <38085822+Majestic95@users.noreply.github.com>
2020-07-08 21:27:41 -0500

    Add files via upload

--- main_test.py
+++ main_test.py
@@ -2,6 +2,7 @@
 import tkinter as tk                # python 3
 from tkinter import font  as tkfont # python 3
 from tkinter import *
+import tkinter.messagebox as msg
 import importlib
 
 from PIL import *
@@ -25,7 +26,6 @@ class BellBankLMS(tk.Tk):
 
         self.title('Bell Bank: Learning Management Software')
         self.geometry("450x450")
-        self.resizable(0, 0)
 
 
         # the container is where we'll stack a bunch of frames
@@ -99,7 +99,7 @@ class page_Training(tk.Frame):
 
         button = tk.Button(self, text="Menu",
                            command=lambda: controller.show_frame("page_Menu"))
-        button.pack()
+        button.pack(expand=1)
 
 
 class page_Quizzes(tk.Frame):
@@ -111,7 +111,7 @@ class page_Quizzes(tk.Frame):
         label.pack(side="top", fill="x", pady=10)
         button = tk.Button(self, text="Menu",
                            command=lambda: controller.show_frame("page_Menu"))
-        button.pack()
+        button.pack(expand=1)
 
 
 class page_Mgmt(tk.Frame):
@@ -125,8 +125,9 @@ class page_Mgmt(tk.Frame):
                            command=lambda: controller.show_frame("page_Menu"))
         button1 = tk.Button(self, text="Training Classes/Rosters", 
                             command=lambda: controller.show_frame("page_Roster"))
-        button.pack()
-        button1.pack()
+
+        button1.pack(expand=1)
+        button.pack(expand=1)
 
 
 class page_Roster(tk.Frame):
@@ -141,41 +142,110 @@ class page_Roster(tk.Frame):
         else:
             self.trainees = trainees
 
+
+        self.trainees_canvas = tk.Canvas(self)
+
+        self.trainees_frame = tk.Frame(self.trainees_canvas)
+        self.text_frame = tk.Frame(self)
+
+        self.trainee_create = tk.Text(self.text_frame, height=3, bg="white", fg="black")
+
+
+        self.scrollbar = tk.Scrollbar(self.trainees_canvas, orient="vertical", command=self.
+            trainees_canvas.yview)
+
+        self.trainees_canvas.configure(yscrollcommand=self.scrollbar.set)
+
+
+        self.trainees_canvas.pack(side="top", fill="both", expand=1)
+        self.scrollbar.pack(side="right", fill="y")
+
+        self.canvas_frame = self.trainees_canvas.create_window((0, 0), window=self.trainees_frame, anchor="n")
+
+        self.trainee_create.pack(side="bottom", fill="x")
+        self.text_frame.pack(side="bottom", fill="x")
+        self.trainee_create.focus_set()
+
+
+
         trainee1 = tk.Label(self, text="---Add Trainee Here---", bg="lightgrey", fg="blue", pady=10)
+        trainee1.bind("<Button-1>", self.remove_trainee)
 
         self.trainees.append(trainee1)
 
         for trainees in self.trainees:
             trainees.pack(side="top", fill="x")
 
-        self.trainee_create = tk.Text(self, height=3, bg="white", fg="black")
-
-        self.trainee_create.pack(side="bottom", fill="x")
-        self.trainee_create.focus_set()
 
         self.trainee_create.bind("<Return>", self.add_trainee)
+        self.bind("<Configure>", self.on_frame_configure)
+        self.bind_all("<MouseWheel>", self.mouse_scroll)
+        self.bind_all("<Button-4>", self.mouse_scroll)
+        self.bind_all("<Button-5>", self.mouse_scroll)
+        self.bind("<Configure>", self.trainee_width)
 
-        self.color_schemes = [{"bg": "lightgrey", "fg": "blue"}, {"bg": "grey", "fg": "white"}]
+
+        self.color_schemes = [{"bg": "lightgrey", "fg": "blue"}, {"bg": "grey", "fg": "blue"}]
 
 
     def add_trainee(self, event=None):
         trainee_name = self.trainee_create.get(1.0,END).strip()
 
         if len(trainee_name) > 0:
-            new_trainee = tk.Label(self, text=trainee_name, pady=10)
+            new_trainee = tk.Label(self, text=trainee_name)
+            self.set_trainee_color(len(self.trainees), new_trainee)
 
-            _, trainee_style_choice = divmod(len(self.trainees), 2)
-
-            my_scheme_choice = self.color_schemes[trainee_style_choice]
-
-            # new_trainee_configure(bg=my_scheme_choice["bg"])
-            # new_trainee_configure(fg=my_scheme_choice["fg"])
-
+            new_trainee.bind("<Button-1>", self.remove_trainee)
             new_trainee.pack(side="top", fill="x")
 
             self.trainees.append(new_trainee)
 
         self.trainee_create.delete(1.0, END)
+        self.trainee_create.mark_set("<Insert>", "1.0")
+
+
+    def remove_trainee(self, event):
+        trainee = event.widget
+        if msg.askyesno("Confirm Deletion", "Delete " + trainee.cget("text") + "?"):
+            self.trainees.remove(event.widget)
+            event.widget.destroy()
+            self.recolor_trainees()
+
+
+    def recolor_trainees(self):
+        for index, trainee in enumerate(self.trainees):
+            self.set_trainee_color(index, trainee)
+
+
+    def set_trainee_color(self, position, trainee):
+        _, trainee_style_choice = divmod(position, 2)
+
+        my_scheme_choice = self.color_schemes[trainee_style_choice]
+
+        trainee.configure(bg=my_scheme_choice["bg"])
+        trainee.configure(fg=my_scheme_choice["fg"])
+
+
+    def on_frame_configure(self, event=None):
+        self.trainees_canvas.configure(scrollregion=self.trainees_canvas.bbox("all"))
+
+
+    def trainee_width(self, event):
+        canvas_width = event.width
+        self.trainees_canvas.itemconfig(self.canvas_frame, width = canvas_width)
+
+
+    def mouse_scroll(self, event):
+        if event.delta:
+            self.trainees_canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+        else:
+            if event.num == 5:
+                move = 1
+            else:
+                move = -1
+
+                self.trainees_canvas.yview_scroll(move, "units")
+
 
 
 if __name__ == "__main__":
