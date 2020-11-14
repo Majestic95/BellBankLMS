6dcc0d605e3da15cc10797222635e6b1b82aa186
Majestic95 <38085822+Majestic95@users.noreply.github.com>
2020-11-13 21:54:53 -0600

    Add files via upload

--- Database.py
+++ Database.py
@@ -1,9 +1,33 @@
 import sqlite3
 
-class Database:
-	def conn(self):
-		print("Database : connection method called")
-		con = sqlite3.connect("trainees.db")
-		cur = con.cursor()
-		cur.execute('CREATE TABLE IF NOT EXISTS Trainees(
-								)')
+class Database():
+
+	def __init__(self, db):
+		"""Init method which returns active connection when instantiated"""
+
+		sql_create_trainees_table = """ CREATE TABLE IF NOT EXISTS trainees (
+											id 			integer 	PRIMARY KEY,
+											firstname 	text 		NOT NULL,
+											lastname 	text 		NOT NULL,
+											traindate 	integer,
+											position 	text,
+											FOREIGN KEY (id)
+											);"""
+
+
+		sql_create_modules_table = """CREATE TABLE IF NOT EXISTS modules (
+											id 			integer 	PRIMARY KEY,
+											title		text		NOT NULL,
+											grade 		integer,
+											);"""
+
+
+		sql_create_curriculums_table = """CREATE TABLE IF NOT EXISTS curriculums (
+											id 			integer		PRIMARY KEY,
+											title 		text		NOT NULL,
+											levelid		integer
+											);"""
+
+
+		try:
+			conn = c
